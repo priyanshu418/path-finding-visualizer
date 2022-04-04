28b964e0eecbe26b629986f35ffa24215ff8c4a2
priyanshu418 <62936070+priyanshu418@users.noreply.github.com>
2022-04-04 17:03:15 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Pvisual from "./Pvisual/Pvisual";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <Pvisual></Pvisual>
+    </div>
+  );
+}
+
+export default App;
